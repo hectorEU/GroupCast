4080843775a76f84b86b0a907a1aefdaf72c2d2c
hectorEU <hmercadovalls@gmail.com>
2017-05-17 12:08:06 +0200

    TOM

--- src/GMS.py
+++ src/GMS.py
@@ -1,9 +1,5 @@
-import random
-import subprocess
-from collections import defaultdict
-from datetime import datetime, timedelta
-
 from pyactor.context import set_context, serve_forever, interval, create_host
+from pyactor.exceptions import TimeoutError
 
 from output import printr
 
@@ -12,48 +8,50 @@ class GMS(object):
     _tell = ["join", "failfix", "run"]
     _ask = ["get_members"]
     _ref = ["join", "get_members"]
+    _parallel = ["failfix"]
 
-    def __init__(self, peers_offer=3, fail_timeout=12):
+    def __init__(self):
         self.peers = {}  # Key: Peer proxy, Value: Connection attempts
-        self.peers_offer = peers_offer  # Max. random sample
-        self.fail_timeout = fail_timeout  # Disconnected peer clean-up
-        self.max_attempt = 5
+        self.fail_detect = 10
+        self.fail_timeout = 2
+        self.max_attempts = 5
 
     # Public actor methods *************************
     def join(self, peer):
-        self.peers[peer] = datetime.now()
+        self.peers[peer] = 0
         printr(self, "Connected: " + peer.get_url())
 
     def get_members(self):
-        members = self.peers.values()
-        return random.sample(members, self.peers_offer if self.peers_offer <= len(members) else len(members))
+        return self.peers
 
     # ***********************************************
 
     # Removes inactive peers from the group
     def failfix(self):
-        old_peers = set(self.peers.keys())
-
-        for peers in self.peers.items():
-
-        for peer in old_peers.difference(self.peers.keys()):
-                printr(self, "Removed: " + peer.get_url())
+        for peer in self.peers:
+            try:
+                if peer.is_alive(timeout=self.fail_timeout):
+                    self.peers[peer] = 0
+            except TimeoutError:
+                self.peers[peer] += 1
+                if self.peers[peer] >= self.max_attempts:
+                    self.peers.pop(peer)
+                    printr(self, "Removed: " + peer.get_url())
 
     def leave(self, peer):
         self.peers.pop(peer)
         printr(self, "Disconnected: " + peer.get_url())
 
-
     # Activates tracker
     def run(self):
-        self.loop = interval(self.host, self.fail_timeout, self.proxy, "failfix")
+        self.loop = interval(self.host, self.fail_detect, self.proxy, "failfix")
 
 
 if __name__ == "__main__":
     set_context()
 
     h = create_host("http://10.21.6.8:6969")
-    tracker = h.spawn("tracker1", GMS)
+    tracker = h.spawn("tracker1", "GMS/GMS")
     tracker.run()
     print "Tracker ready"
 
--- src/Peer.py
+++ src/Peer.py
@@ -1,39 +1,44 @@
-from Sequencer import Sequencer
 from collections import OrderedDict
+
+from pyactor.context import set_context, serve_forever, create_host
+
 from GMS import GMS
-from pyactor.context import set_context, serve_forever, interval, create_host
 
 
 class Peer(object):
-    _tell = ["join", "multicast", "run"]
+    _tell = ["join", "multicast", "run", "process_msg", "receive", "set_leader"]
+    _ask = ["is_alive", "request"]
+    _ref = ["receive", "is_alive", "request"]
 
     def __init__(self):
-        self.seq = Sequencer()
         self.queue = OrderedDict()
-        self.peers = []
+        self.seq = 0
 
-    def __hash__(self):
-        return self.get_url()
-
-    def __eq__(self, other):
-        return self.get_url() == other.get_url()
-
-    def __ne__(self, other):
-        return not self.__eq__(self, other)
+    def request(self):
+        self.seq += 1
+        return self.seq
 
     def multicast(self, msg):
-        id = self.seq.request()
-        for peer in self.peers:
-            peer.receive(msg, id)
+        seq = self.leader.request()
+        for peer in self.gms.get_members():
+            peer.receive(seq, msg)
 
-    def receive(self, msg, id):
-        self.queue[id] = msg
+    def receive(self, seq, msg):
+        if self.seq == seq:
+            self.process_msg(msg)
+            self.seq += 1
+            fifo = list(self.queue)
+            while len(fifo) is not 0 and self.seq == fifo[0][0]:
+                self.process_msg(fifo.pop())
+                self.seq += 1
+        else:
+            self.queue[seq] = msg
 
-    def process_msg(self):
-        keys = self.queue.keys()
-        if keys[1] > keys[0]+1:
-            raise LookupError
-        return self.queue.pop(0)
+    def process_msg(self, msg):
+        print msg
+
+    def is_alive(self):
+        return True
 
     def join(self):
         self.gms.join(self.proxy)
@@ -41,14 +46,25 @@ class Peer(object):
     def run(self):
         self.gms = self.host.lookup_url('http://10.21.6.8:6969/tracker1', GMS)
 
+    def set_leader(self, peer):
+        self.leader = peer
+
+
 if __name__ == "__main__":
     set_context()
 
-    h = create_host("http://10.21.6.8:17969")
+    h = create_host("http://10.21.6.8:26969")
 
     p = h.spawn("peer1", "Peer/Peer")
     p.run()
     p.join()
-    # p.multicast("hola")
+    p2 = h.spawn("peer2", "Peer/Peer")
+    p2.run()
+    p2.join()
+
+    p.set_leader(p)
+    p2.set_leader(p)
+
+    p2.multicast("hola")
 
     serve_forever()
